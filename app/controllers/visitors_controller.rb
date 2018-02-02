7cf210a043c96c36df7491da23dca9a5e71f84ec
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-02-02 13:33:46 +0000

    automatically backed up by learn

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -30,32 +30,32 @@ class VisitorsController < ApplicationController
     end
    end
 
-   patch '/visitor/:id/cities' do
-     @visitor = Visitor.find_by_id(params[:id])
-     if logged_in? && @visitor.id == session[:id]
-     @visitor.city_ids = params[:cities]
-     #### making it so a country is added to a profile if a pre-existing city is selected.
-     @visitor.city_ids.each do |city|
-       city_object = City.find_by_id(city)
-       if !@visitor.countrys.include?(Country.find_by(:id => city_object.country_id))
-       @visitor.countrys << Country.find_by(:id => city_object.country_id)
+     patch '/visitor/:id/cities' do
+       @visitor = Visitor.find_by_id(params[:id])
+        if logged_in? && @visitor.id == session[:id]
+          @visitor.city_ids = params[:cities]
+       #### making it so a country is added to a profile if a pre-existing city is selected.
+          @visitor.city_ids.each do |city|
+            city_object = City.find_by_id(city)
+         if !@visitor.countrys.include?(Country.find_by(:id => city_object.country_id))
+         @visitor.countrys << Country.find_by(:id => city_object.country_id)
+        end
+       end
+
+       if params[:city_name] != ""
+       @city = City.find_or_create_by(:name => params[:city_name], :country_id => params[:countries])
+       @city.save
+        if !@visitor.cities.include?(@city)
+         @visitor.cities << @city
+        end
+        if !@visitor.countrys.include?(Country.find_by(:id => params[:countries]))
+          @visitor.countrys << Country.find_by(:id => params[:countries])
+        end
+        end
+       @visitor.save
+       redirect to "/visitor/#{@visitor.id}"
       end
      end
-
-     if params[:city_name] != ""
-     @city = City.find_or_create_by(:name => params[:city_name], :country_id => params[:countries])
-     @city.save
-      if !@visitor.cities.include?(@city)
-       @visitor.cities << @city
-      end
-      if !@visitor.countrys.include?(Country.find_by(:id => params[:countries]))
-        @visitor.countrys << Country.find_by(:id => params[:countries])
-      end
-      end
-     @visitor.save
-     redirect to "/visitor/#{@visitor.id}"
-    end
-   end
 
    get '/visitors' do
      erb :'/visitors/visitors'
